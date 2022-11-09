b2f7078569f9fa22b8d4f4ffde407ba28b914a05
Filip Škorpík <filipskorpik3@gmail.com>
2022-11-09 13:14:41 +0100

    scroll-home

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
-
-$(document).ready(function(){
-    
-    new WOW().init();
-
-  
-  
-  
-  
-  
-  
-  
-  
-    
-        
+new WOW().init();
 
 
+window.addEventListener("scroll", function(){
+    let scrolled = window.scrollY
+    let scrollable = document.documentElement.scrollHeight - window.innerHeight
+    let scrollHome = document.querySelector(".scroll-home")
 
-
-});
+    if(Math.ceil(scrolled) > (scrollable/4)){
+        scrollHome.classList.add("scroll-home__is-active")
+    } else {
+        scrollHome.classList.remove("scroll-home__is-active")
+    }
+})
